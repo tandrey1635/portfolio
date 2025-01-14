82e8a12e00f7a7751e1c566ebc069ce45822287b
tarakanovandrej1635 <tarakanovandrej1635@gmail.com>
2025-01-15 00:38:26 +0300

    Табы в моих работах вкладка html дизайн

--- src/components/potfolio-tabs/PotfolioTabs.jsx
+++ src/components/potfolio-tabs/PotfolioTabs.jsx
@@ -6,7 +6,7 @@ const PortfolioTabs = (props) => {
     return(
         <ul className="portfolio__tabs">
             <li className={props.tabAll ? "portfolio__tabs__items active" : "portfolio__tabs__items"} onClick={props.all}>Все</li>
-            <li className={props.tabHtml ? "portfolio__tabs__items active" : "portfolio__tabs__items"} onClick={props.html}>Html/Css</li>
+            <li className={props.tabHtml ? "portfolio__tabs__items active_html" : "portfolio__tabs__items"} onClick={props.html}>Html/Css</li>
             <li className={props.tabBootstrap ? "portfolio__tabs__items active" : "portfolio__tabs__items"} onClick={props.bootstrap}>Bootstrap</li>
             <li className={props.tabJavascript ? "portfolio__tabs__items active" : "portfolio__tabs__items"} onClick={props.javascript}>Js</li>
             <li className={props.tabReact ? "portfolio__tabs__items active" : "portfolio__tabs__items"} onClick={props.react}>React</li>
